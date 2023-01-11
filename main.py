6bea24347d9be634deabb8677211e984821b69a6
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-11 18:23:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 from  pycreate2 import Create2
 import time
 
-# Create a Create2.
-# FIX THIS CAMERON PLEASE THIS IS THE ISSUE
-port = "/dev/tty.usbserial-DA01NUGW"  # where is your serial port?
+# Setup
+port = "/dev/tty.usbserial-DA01NUGW"   # Where is the Serial Port? 
 bot = Create2(port)
-
-# Start the Create 2
 bot.start()
 
-# Put the Create2 into 'safe' mode so we can drive it
-# This will still provide some protection
+# Launches the bot in safe mode
+# RECCOMENDED MODE!
 bot.safe()
 
-# You are responsible for handling issues, no protection/safety in
-# this mode ... becareful
-#bot.full()
+# ONLY run in sudo mode if you know what you're doing
+# bot.full()
 
-# directly set the motor speeds ... move forward
+# Moves the robot forwards
 bot.drive_direct(100, 100)
 time.sleep(2)
 
-# turn in place
-bot.drive_direct(200,-200)  # inputs for motors are +/- 500 max
+# Turns the robot (In place)
+bot.drive_direct(200,-200)  # +/- 500 max
 time.sleep(2)
 
 # Stop the bot
 bot.drive_stop()
 
-# query some sensors
+# Query some sensors
 #sensors = bot.get_sensors()  # returns all data
 #print(sensors.light_bumper_left)
 
